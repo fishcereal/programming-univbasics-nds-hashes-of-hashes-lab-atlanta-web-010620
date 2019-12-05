72868406b544b11c23f74b1f26db90974c99435c
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 18:29:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,9 +45,9 @@ def naming_system
           label: "Genus",
           sub_category: {
             label: "Species",
-             sub_category: {
-               nil}
-             }
+             sub_category: 
+             nil
+             
           }
         }
       }
